7ca81c196493eee3732c34ed9d0c9c2da9820a73
Vera Gorodnicheva <vera.develop@yandex.ru>
2022-01-22 18:43:28 +0300

    migrations + seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(UserRolesTableSeeder::class);
+        $this->call(AttributeGroupsSeeder::class);
+        $this->call(AttributeProductsSeeder::class);
+        $this->call(AttributeValuesSeeder::class);
+        $this->call(BrandsSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(CurrenciesSeeder::class);
+        $this->call(GalleriesSeeder::class);
+        $this->call(ProductsSeeder::class);
+        $this->call(RelatedProductsSeeder::class);
+        $this->call(OrdersSeeder::class);
+        $this->call(AdminOrderProductsSeeder::class);
     }
 }
